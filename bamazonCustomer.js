744db52d9a219a5f2810df4a56a04e5cac7fffb2
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-09-06 19:00:12 -0400

    connects to db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,64 +22,90 @@ function selectAll() {
     });
 };
 
-function orderInput (){
- inquirer.prompt ([
-   {
-      type: "input",
-      message: "Please enter product ID",
-      name: "id"
-     },
-      {
-      type: "input",
-      message: "Please enter quantity",
-      name: "quantity"
-    },
-   ])
-  .then(function(result) {
-   connection.query("SELECT * FROM products WHERE item_id=?", [result.id], function(err, res) {
+function orderInput() {
+    inquirer.prompt([{
+                type: "input",
+                message: "Please enter product ID",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Please enter quantity",
+                name: "quantity"
+            },
+        ])
+        .then(function(result) {
+            connection.query("SELECT * FROM products WHERE item_id=?", [result.id], function(err, res) {
 
-   for (var i = 0; i < res.length; i++) {
+                for (var i = 0; i < res.length; i++) {
 
-   var price = (result.quantity * res[i].price);
-   var roundedPrice = Math.round(price*100)/100;
-   var displayPrice = "$" + roundedPrice;
-  
-   
-   var item = res[i].product_name;
-   
-   var itemID = result.id;
-   
-   var amountRequested = result.quantity;
-   
-   var amountInStock = res[i].stock_quantity;
-    
-       if (amountRequested > amountInStock){
-   console.log("Insufficient Quantity");
-   
-   orderInput();
-   }
-   else {
-       placeOrder(item, amountRequested, amountInStock, displayPrice, itemID);
-     }
-      
-    }
-    })
-  });
+                    var price = (result.quantity * res[i].price);
+                    var roundedPrice = Math.round(price * 100) / 100;
+                    var displayPrice = "$" + roundedPrice;
+
+
+                    var item = res[i].product_name;
+
+                    var itemID = result.id;
+
+
+                    var amountRequested = result.quantity;
+
+                    var amountInStock = res[i].stock_quantity;
+
+                    if (amountRequested > amountInStock) {
+                        console.log("Insufficient Quantity");
+
+                        orderInput();
+                    } else {
+                        placeOrder(item, amountRequested, amountInStock, displayPrice, itemID);
+                    }
+
+                }
+            })
+        });
 };
 
-function placeOrder(item, amountRequested, amountInStock, displayPrice, itemID){  
- var order = {
-   item: item,
-   amount: amountRequested,
-   price: displayPrice,
-   id: itemID
- }
-  var orderArr = [];
- orderArr.push(order);
- console.log("Your order: ")
- console.log(orderArr);
+function placeOrder(item, amountRequested, amountInStock, displayPrice, itemID) {
+    var order = {
+        item: item,
+        quantity: amountRequested,
+        price: displayPrice,
+        id: itemID
+    }
+    var orderArr = [];
+    orderArr.push(order);
+    console.log("Your order: ")
+    console.log(orderArr);
+    updateDatebase(amountRequested, amountInStock, itemID);
 }
 
-function updateDatebase (item, amountRequested, amountInStock, displayPrice, itemID){
+function updateDatebase(amountRequested, amountInStock, itemID) {
+    var newQuantity = (amountInStock - amountRequested);
+    console.log("new quantity" + newQuantity);
+    console.log("id" + itemID);
+
+// UPDATE products
+// SET stock_quantity = newQuantity
+// WHERE item_id = result.id;
+
+    // connection.query("UPDATE products SET stock_quantity = ? WHERE item_id=?" , [newQuantity],[itemID], function(err, res) {})
+          
+              connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: newQuantity
+              },
+              {
+                item_id: itemID
+              }
+            ],
+            function(error) {
+              if (error) throw error;
+            }
+          );
+
+      //Only working sql so far: SELECT * FROM bamazon_db.products;
 
 }
